1f1e523a5c5655c52b394252bf5b23a0d252cff0
ayanrndm <gabrodrigues0702@gmail.com>
2024-04-12 12:28:50 -0300

    ProjetoDoSiteNovo

---
+++ main.js
@@ -0,0 +1,14 @@
+
+
+function ExibirTextoNaTabela(id, ){
+    let campo = document.querySelector(id)
+    campo.innerHTML = texto;
+}
+function ValidarValor() {
+    let chute = document.getElementById('name').value;
+    ExibirTextoNaTabela('pc', chute)
+    
+}
+function 
+
+
